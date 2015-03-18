c972a0912c8b06fccc294668e67462f9173ad43a
Guillaume Claret <dev@clarus.me>
2015-03-18 17:35:25 +0100

    Counter effect added

--- src/Main.v
+++ src/Main.v
@@ -1,5 +1,21 @@
 Require Import Io.All.
 
+Module Counter.
+  Inductive t : Set :=
+  | Read
+  | Incr.
+
+  Definition answer (command : t) : Type :=
+    match command with
+    | Read => nat
+    | Incr => unit
+    end.
+
+  Definition effect : Effects.t := {|
+    Effects.command := t;
+    Effects.answer := answer |}.
+End Counter.
+
 Module BinaryTree.
   Inductive t (A : Type) : Type :=
   | Leaf : t A
@@ -7,15 +23,20 @@ Module BinaryTree.
   Arguments Leaf [A].
   Arguments Node [A] _ _ _.
 
-  Fixpoint tag_aux {A : Type} (i : nat) (tree : t A) : nat * t (nat * A) :=
-    match tree with
-    | Leaf => (i, Leaf)
-    | Node tree_left x tree_right =>
-      let (i_left, tree_left) := tag_aux i tree_left in
-      let (i_right, tree_right) := tag_aux (i_left + 1) tree_right in
-      (i_right, Node tree_left (i_left, x) tree_right)
-    end.
+  Module Functional.
+    Fixpoint tag_aux {A : Type} (i : nat) (tree : t A) : nat * t (nat * A) :=
+      match tree with
+      | Leaf => (i, Leaf)
+      | Node tree_left x tree_right =>
+        let (i_left, tree_left) := tag_aux i tree_left in
+        let (i_right, tree_right) := tag_aux (i_left + 1) tree_right in
+        (i_right, Node tree_left (i_left, x) tree_right)
+      end.
 
-  Definition tag {A : Type} (i : nat) (tree : t A) : t (nat * A) :=
-    snd (tag_aux i tree).
+    Definition tag {A : Type} (i : nat) (tree : t A) : t (nat * A) :=
+      snd (tag_aux i tree).
+  End Functional.
+
+  Module Imperative.
+  End Imperative.
 End BinaryTree.
