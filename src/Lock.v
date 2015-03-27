55f9f9a8842435a7c1d424c69712c32a918cc4f9
Guillaume Claret <dev@clarus.me>
2015-03-27 11:44:40 +0100

    Definition of the lock function

--- src/Lock.v
+++ src/Lock.v
@@ -3,16 +3,29 @@ Require Import Io.All.
 Import C.Notations.
 
 Module Command.
-  Inductive t (A : Type) :=
-  | Lock (id : A)
-  | Unlock (id : A).
+  Inductive t (L : Type) :=
+  | Lock (l : L)
+  | Unlock (l : L).
+  Arguments Lock {L} _.
+  Arguments Unlock {L} _.
 End Command.
 
-Definition answer {A : Type} (c : Command.t A) : Type :=
+Definition answer {L : Type} (c : Command.t L) : Type :=
   match c with
   | Command.Lock _ => unit
   | Command.Unlock _ => unit
   end.
 
-Definition effect (A : Type) : Effect.t :=
-  Effect.New (Command.t A) answer.
+Definition effect (L : Type) : Effect.t :=
+  Effect.New (Command.t L) answer.
+
+Definition lock {L A : Type} (l : L) (x : C.t (effect L) A)
+  : C.t (effect L) A :=
+  do! call (effect L) (Command.Lock l) in
+  let! x := x in
+  do! call (effect L) (Command.Unlock l) in
+  ret x.
+
+Module LockFree.
+  Definition C := C.t (effect bool).
+End LockFree.
