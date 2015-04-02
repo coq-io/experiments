7f892768f6286a1d15d089c5a0b6ae022c66d88d
Guillaume Claret <dev@clarus.me>
2015-04-02 17:47:58 +0200

    More draft about equivalence with DPN

--- src/Callbacks.v
+++ src/Callbacks.v
@@ -7,18 +7,38 @@ Module Effect.
 End Effect.
 
 Module C.
-  Inductive t (E : Effect.t) (A : Type) : Type :=
-  | Ret : A -> t E A
-  | Call : forall (command : Effect.command E),
-    (Effect.answer E command -> t E A) -> t E A
-  | Spawn : t E A -> t E unit -> t E A.
+  Inductive t (E : Effect.t) : Type :=
+  | Ret : t E
+  | Call : forall (c : Effect.command E), (Effect.answer E c -> t E) -> t E
+  | Fork : t E -> t E -> t E.
+  Arguments Ret {E}.
+  Arguments Call {E} _ _.
+  Arguments Fork {E} _ _.
 End C.
 
 Module DPN.
-  Record t := New {
-    Act : Type;
-    P : Type;
-    G : Type;
-    seq_step : P -> G -> Act -> P -> list G -> Prop;
-    par_step : P -> G -> Act -> P -> list G -> P -> list G -> Prop }.
+  Record t (State Action : Type) := New {
+    seq_step : State -> Action -> State -> Prop;
+    par_step : State -> Action -> State -> State -> Prop }.
+
+  Definition empty {State Action : Type} : t State Action := {|
+    seq_step := fun _ _ _ => False;
+    par_step := fun _ _ _ _ => False |}.
+
+  Module Action.
+    Inductive t (E : Effect.t) :=
+    | Call : forall (c : Effect.command E), Effect.answer E c -> t E
+    | Fork : t E.
+    Arguments Call {E} _ _.
+    Arguments Fork {E}.
+  End Action.
+
+  Definition call {E : Effect.t} (c : Effect.command E)
+    (callback : Effect.answer E c -> C.t E) : t (C.t E) (Action.t E) := {|
+    seq_step := fun x action y =>
+      exists a : Effect.answer E c,
+        x = C.Call c callback /\
+        action = Action.Call c a /\
+        y = callback a;
+    par_step := fun _ _ _ _ => False |}.
 End DPN.
