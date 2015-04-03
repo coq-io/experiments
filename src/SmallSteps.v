6465610107c36ef2a681ca23d3e7a82ce0a44744
Guillaume Claret <dev@clarus.me>
2015-04-03 16:42:20 +0200

    Proof of the non-blocking property for small-steps with a state

--- src/SmallSteps.v
+++ src/SmallSteps.v
@@ -67,43 +67,92 @@ Module SmallStep.
   Qed.
 End SmallStep.
 
+Module StateSmallStep.
+  Inductive t {E : Effect.t} {S : Type}
+    (answer : forall c, S -> Effect.answer E c)
+    (state : Effect.command E -> S -> S) : forall {A : Type},
+    C.t E A -> S -> C.t E A -> S -> Prop :=
+  | Call : forall (c : Effect.command E) (s : S),
+    t answer state (C.Call c) s (C.Ret _ (answer c s)) (state c s)
+  | LetLeft : forall (A B : Type) (x : C.t E A) (f : A -> C.t E B)
+    (x' : C.t E A) (s s' : S),
+    t answer state x s x' s' ->
+    t answer state (C.Let _ _ x f) s (C.Let _ _ x' f) s'
+  | Let : forall (A B : Type) (x : C.t E A) (f : A -> C.t E B) (v_x : A)
+    (s : S),
+    t answer state (C.Let _ _ (C.Ret _ v_x) f) s (f v_x) s
+  | JoinLeft : forall (A B : Type) (x : C.t E A) (y : C.t E B) (x' : C.t E A)
+    (s s' : S),
+    t answer state x s x' s' ->
+    t answer state (C.Join _ _ x y) s (C.Join _ _ x' y) s'
+  | JoinRight : forall (A B : Type) (x : C.t E A) (y : C.t E B) (y' : C.t E B)
+    (s s' : S),
+    t answer state y s y' s' ->
+    t answer state (C.Join _ _ x y) s (C.Join _ _ x y') s'
+  | Join : forall (A B : Type) (v_x : A) (v_y : B) (s : S),
+    t answer state (C.Join _ _ (C.Ret _ v_x) (C.Ret _ v_y)) s (C.Ret _ (v_x, v_y)) s
+  | FirstLeft : forall (A B : Type) (x : C.t E A) (y : C.t E B) (x' : C.t E A)
+    (s s' : S),
+    t answer state x s x' s' ->
+    t answer state (C.First _ _ x y) s (C.First _ _ x' y) s'
+  | FirstRight : forall (A B : Type) (x : C.t E A) (y : C.t E B) (y' : C.t E B)
+    (s s' : S),
+    t answer state y s y' s' ->
+    t answer state (C.First _ _ x y) s (C.First _ _ x y') s'
+  | FirstInl : forall (A B : Type) (v_x : A) (y : C.t E B) (s : S),
+    t answer state (C.First _ _ (C.Ret _ v_x) y) s (C.Ret _ (inl v_x)) s
+  | FirstInr : forall (A B : Type) (x : C.t E A) (v_y : B) (s : S),
+    t answer state (C.First _ _ x (C.Ret _ v_y)) s (C.Ret _ (inr v_y)) s.
 
-Module State.
-  Module SmallStep.
-    Inductive t {E : Effect.t} {S : Type}
-      (answer : forall c, S -> Effect.answer E c)
-      (state : Effect.command E -> S -> S) : forall {A : Type},
-      C.t E A -> S -> C.t E A -> S -> Prop :=
-    | Call : forall (c : Effect.command E) (s : S),
-      t answer state (C.Call c) s (C.Ret _ (answer c s)) (state c s)
-    | LetLeft : forall (A B : Type) (x : C.t E A) (f : A -> C.t E B)
-      (x' : C.t E A) (s s' : S),
-      t answer state x s x' s' ->
-      t answer state (C.Let _ _ x f) s (C.Let _ _ x' f) s'
-    | Let : forall (A B : Type) (x : C.t E A) (f : A -> C.t E B) (v_x : A)
-      (s : S),
-      t answer state (C.Let _ _ (C.Ret _ v_x) f) s (f v_x) s
-    | JoinLeft : forall (A B : Type) (x : C.t E A) (y : C.t E B) (x' : C.t E A)
-      (s s' : S),
-      t answer state x s x' s' ->
-      t answer state (C.Join _ _ x y) s (C.Join _ _ x' y) s'
-    | JoinRight : forall (A B : Type) (x : C.t E A) (y : C.t E B) (y' : C.t E B)
-      (s s' : S),
-      t answer state y s y' s' ->
-      t answer state (C.Join _ _ x y) s (C.Join _ _ x y') s'
-    | Join : forall (A B : Type) (v_x : A) (v_y : B) (s : S),
-      t answer state (C.Join _ _ (C.Ret _ v_x) (C.Ret _ v_y)) s (C.Ret _ (v_x, v_y)) s
-    | FirstLeft : forall (A B : Type) (x : C.t E A) (y : C.t E B) (x' : C.t E A)
-      (s s' : S),
-      t answer state x s x' s' ->
-      t answer state (C.First _ _ x y) s (C.First _ _ x' y) s'
-    | FirstRight : forall (A B : Type) (x : C.t E A) (y : C.t E B) (y' : C.t E B)
-      (s s' : S),
-      t answer state y s y' s' ->
-      t answer state (C.First _ _ x y) s (C.First _ _ x y') s'
-    | FirstInl : forall (A B : Type) (v_x : A) (y : C.t E B) (s : S),
-      t answer state (C.First _ _ (C.Ret _ v_x) y) s (C.Ret _ (inl v_x)) s
-    | FirstInr : forall (A B : Type) (x : C.t E A) (v_y : B) (s : S),
-      t answer state (C.First _ _ x (C.Ret _ v_y)) s (C.Ret _ (inr v_y)) s.
-  End SmallStep.
-End State.
+  Fixpoint non_blocking {E : Effect.t} {S : Type}
+    (answer : forall c, S -> Effect.answer E c)
+    (state : Effect.command E -> S -> S) {A : Type} (x : C.t E A) (s : S)
+    {struct x} : (exists v_x : A, x = C.Ret _ v_x) \/
+      (exists x' : C.t E A, exists s' : S, t answer state x s x' s').
+    destruct x as [A v_x | c | A B x f | A B x y | A B x y].
+    - left.
+      now exists v_x.
+    - right.
+      exists (C.Ret _ (answer c s)).
+      exists (state c s).
+      apply Call.
+    - right.
+      destruct (non_blocking _ _ answer state _ x s) as [H | H].
+      + destruct H as [v_x H]; rewrite H.
+        exists (f v_x).
+        exists s.
+        now apply Let.
+      + destruct H as [x' H]; destruct H as [s' H].
+        exists (C.Let _ _ x' f).
+        exists s'.
+        now apply LetLeft.
+    - right.
+      destruct (non_blocking _ _ answer state _ x s) as [H_x | H_x].
+      + destruct H_x as [v_x H_x].
+        destruct (non_blocking _ _ answer state _ y s) as [H_y | H_y].
+        * destruct H_y as [v_y H_y].
+          exists (C.Ret _ (v_x, v_y)).
+          exists s.
+          rewrite H_x; rewrite H_y.
+          apply Join.
+        * destruct H_y as [y' H_y]; destruct H_y as [s' H_y].
+          exists (C.Join _ _ x y').
+          exists s'.
+          now apply JoinRight.
+      + destruct H_x as [x' H_x]; destruct H_x as [s' H_x].
+        exists (C.Join _ _ x' y).
+        exists s'.
+        now apply JoinLeft.
+    - right.
+      destruct (non_blocking _ _ answer state _ x s) as [H_x | H_x].
+      + destruct H_x as [v_x H_x].
+        exists (C.Ret _ (inl v_x)).
+        exists s.
+        rewrite H_x.
+        apply FirstInl.
+      + destruct H_x as [x' H_x]; destruct H_x as [s' H_x].
+        exists (C.First _ _ x' y).
+        exists s'.
+        now apply FirstLeft.
+  Qed.
+End StateSmallStep.
