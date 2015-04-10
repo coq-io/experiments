eca7ec37b6cdab5c86cee06bd349f54871302ce5
Guillaume Claret <dev@clarus.me>
2015-04-10 19:48:50 +0200

    Last draft

--- src/SimpleSmallSteps.v
+++ src/SimpleSmallSteps.v
@@ -24,9 +24,40 @@ Module Sequential.
 End Sequential.
 
 Module Concurrent.
-  Inductive t (E : Effect.t) : Type :=
-  | Ret : Sequential.t E -> t E
-  | Spawn : t E -> Sequential.t E -> t E.
-  Arguments Ret {E} _.
-  Arguments Spawn {E} _ _.
+  Definition t (E : Effect.t) := list (Sequential.t E).
 End Concurrent.
+
+Module Choose.
+  Inductive t (E : Effect.t) (S : Type) : Type :=
+  | Ret : t E S
+  | Call : Effect.command E -> (S -> t E S) -> t E S
+  | Choose : t E S -> t E S -> t E S.
+  Arguments Ret {E S}.
+  Arguments Call {E S} _ _.
+  Arguments Choose {E S} _ _.
+
+  Fixpoint compile {E S} m (x : Sequential.t E) (y : t E S) {struct y} : t E S :=
+    let fix aux (x : Sequential.t E) {struct x} : t E S :=
+      match (x, y) with
+      | (Sequential.Ret, y) => y
+      | (Sequential.Call c h, Ret) =>
+        Call c (fun s => compile m (h (Model.answer m c s)) y)
+      | (Sequential.Call c h, Call c' h') =>
+        Choose
+          (Call c (fun s => compile m (h (Model.answer m c s)) y))
+          (Call c' (fun s' => compile m x (h' s')))
+      | (Sequential.Call c h, Choose y1 y2) =>
+        Choose (compile m x y1) (compile m x y2)
+      end in
+    match (x, y) with
+    | (Sequential.Ret, y) => y
+    | (Sequential.Call c h, Ret) =>
+      Call c (fun s => compile m (h (Model.answer m c s)) y)
+    | (Sequential.Call c h, Call c' h') =>
+      Choose
+        (Call c (fun s => compile m (h (Model.answer m c s)) y))
+        (Call c' (fun s' => compile m x (h' s')))
+    | (Sequential.Call c h, Choose y1 y2) =>
+      Choose (compile m x y1) (compile m x y2)
+    end.
+End Choose.
