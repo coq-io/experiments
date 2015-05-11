d6f968d835e272ad0dd528cb9e13d09d29305978
Guillaume Claret <dev@clarus.me>
2015-05-11 16:07:24 +0200

    Definition of answers

--- src/SuperMarket.v
+++ src/SuperMarket.v
@@ -1,4 +1,23 @@
 Require Import Io.All.
+Require Import ListString.All.
+
+Module Article.
+  Record t : Set := New {
+    name : LString.t;
+    price : nat }.
+End Article.
+
+Module LoyaltyCard.
+  Inductive t : Set :=
+  | NoCard
+  | MarketCard.
+End LoyaltyCard.
+
+Module PaymentMethod.
+  Inductive t : Set :=
+  | Cash
+  | Credit.
+End PaymentMethod.
 
 Module Command.
   Inductive t : Set :=
@@ -9,4 +28,15 @@ Module Command.
   | GetCash (n : nat)
   | GetCredit (n : nat)
   | Goodbye.
+
+  Definition answer (c : t) : Type :=
+    match c with
+    | Welcome => unit
+    | ScanArticle => option Article.t
+    | GetLoyaltyCard => LoyaltyCard.t
+    | GetPaymentMethod => PaymentMethod.t
+    | GetCash _ => unit
+    | GetCredit _ => unit
+    | Goodbye => unit
+    end.
 End Command.
