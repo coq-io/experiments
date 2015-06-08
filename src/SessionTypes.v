fd0544f1b6155a6492ce98928da3fe606975354d
Guillaume Claret <dev@clarus.me>
2015-06-08 15:54:44 +0200

    Validity predicate for session types

--- src/SessionTypes.v
+++ src/SessionTypes.v
@@ -5,6 +5,69 @@ Module Session.
   | Ret : forall (A : Type), t E A
   | Call : forall (c : Effect.command E), t E (Effect.answer E c)
   | Let : forall (A B : Type), t E A -> (A -> t E B) -> t E B
-  | Choose : forall (A : Type), t E A + t E A -> t E A
+  | Choose : forall (A : Type), t E A -> t E A -> t E A
   | Join : forall (A B : Type), t E A -> t E B -> t E (A * B).
+  Arguments Ret {E} _.
+  Arguments Call {E} _.
+  Arguments Let {E A B} _ _.
+  Arguments Choose {E A} _ _.
+  Arguments Join {E A B} _ _.
+
+  Fixpoint eval {E : Effect.t} {A : Type} (x : C.t E A) : t E A :=
+    match x with
+    | C.Ret A _ => Ret A
+    | C.Call c => Call c
+    | C.Let _ _ x f => Let (eval x) (fun v => eval (f v))
+    | C.Choose _ x1 x2 => Choose (eval x1) (eval x2)
+    | C.Join _ _ x y => Join (eval x) (eval y)
+    end.
+
+  Module Valid.
+    Inductive t {E} : forall {A}, C.t E A -> Session.t E A -> Prop :=
+    | Ret : forall A (x : A), t (C.Ret _ x) (Session.Ret A)
+    | Call : forall c, t (C.Call c) (Session.Call c)
+    | Let : forall A B (x : C.t E A) (f : A -> C.t E B) (s_x : Session.t E A)
+      (s_f : A -> Session.t E B), t x s_x -> (forall v, t (f v) (s_f v)) ->
+      t (C.Let _ _ x f) (Session.Let s_x s_f)
+    | Choose : forall A (x1 x2 : C.t E A) (s1 s2 : Session.t E A),
+      t x1 s1 -> t x2 s2 -> t (C.Choose _ x1 x2) (Session.Choose s1 s2)
+    | Join : forall A B (x : C.t E A) (y : C.t E B) (s_x : Session.t E A)
+      (s_y : Session.t E B), t x s_x -> t y s_y ->
+      t (C.Join _ _ x y) (Session.Join s_x s_y).
+  End Valid.
 End Session.
+
+Module Example.
+  Require Import Coq.Lists.List.
+  Require Import Coq.Strings.Ascii.
+  Require Import Io.All.
+  Require Import Io.System.All.
+  Require Import ListString.All.
+
+  Import ListNotations.
+  Import C.Notations.
+  Local Open Scope char.
+
+  Definition your_name (argv : list LString.t) : C.t System.effect unit :=
+    do! System.log (LString.s "What is your name?") in
+    let! name := System.read_line in
+    match name with
+    | None => ret tt
+    | Some name => System.log (LString.s "Hello " ++ name ++ LString.s "!")
+    end.
+
+  (* Compute Session.eval (your_name []). *)
+
+  (*Definition your_name_session
+    : Session.eval (your_name []) =
+      Session.Let (Session.eval (System.log _)) (fun _ =>
+      Session.Let (Session.eval System.read_line) (fun name =>
+      match name with
+      | None => _
+      | Some name => _
+      end))
+    := eq_refl.*)
+
+  (*Definition your_name_session : Session.t System.effect unit :=
+    Session.Let (Session.Call )*)
+End Example.
