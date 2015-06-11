21e104651a989d88767049c4ed150fcac1e1058c
Guillaume Claret <dev@clarus.me>
2015-06-11 17:27:43 +0200

    Successful run for the ATM

--- src/Atm.v
+++ src/Atm.v
@@ -35,6 +35,30 @@ Definition main : C.t E unit :=
   let! password := call E Command.GetPassword in
   let! is_authorized := call E (Command.CheckAuthorization login password) in
   if is_authorized then
-    ret tt
+    let! amount := call E Command.GetWithdrawAmount in
+    let! is_withdraw_ok := call E (Command.DoWithdraw amount) in
+    if is_withdraw_ok then
+      let! is_money_ok := call E (Command.GiveMoney amount) in
+      if is_money_ok then
+        ret tt
+      else
+        call E (Command.DisplayError (LString.s "Cannot give you the money."))
+    else
+      call E (Command.DisplayError (LString.s "Cannot withdraw the money."))
   else
     call E (Command.DisplayError (LString.s "Wrong password.")).
+
+Module Run.
+  Import Io.Run.
+
+  Definition main_ok (login password : LString.t) (amount : N) : Run.t main tt.
+    eapply Let; [apply (Call E Command.GetLogin login) |].
+    eapply Let; [apply (Call E Command.GetPassword password) |].
+    eapply Let;
+      [apply (Call E (Command.CheckAuthorization login password) true) |].
+    eapply Let; [apply (Call E Command.GetWithdrawAmount amount) |].
+    eapply Let; [apply (Call E (Command.DoWithdraw amount) true) |].
+    eapply Let; [apply (Call E (Command.GiveMoney amount) true) |].
+    apply Ret.
+  Defined.
+End Run.
