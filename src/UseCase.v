167907764f8f06fabd07a5140758e442fe038f3c
Guillaume Claret <dev@clarus.me>
2015-06-22 14:36:56 +0200

    Definition of a join operation

--- src/UseCase.v
+++ src/UseCase.v
@@ -16,12 +16,12 @@ Module Le.
     forall (p1 : parameter u1), exists (p2 : parameter u2),
       JMeq (run u1 p1) (run u2 p2).
 
-  Definition reflexive {E A} {x : C.t E A} (u : UseCase.t x) : t u u.
+  Lemma reflexive {E A} {x : C.t E A} (u : UseCase.t x) : t u u.
     intro p; exists p.
     apply JMeq_refl.
   Qed.
 
-  Definition transitivity {E A} {x : C.t E A} {u1 u2 u3 : UseCase.t x}
+  Lemma transitivity {E A} {x : C.t E A} {u1 u2 u3 : UseCase.t x}
     (H12 : t u1 u2) (H23 : t u2 u3) : t u1 u3.
     intro p1.
     destruct (H12 p1) as [p2 Heq12].
@@ -30,3 +30,45 @@ Module Le.
     now apply JMeq_trans with (y := run u2 p2).
   Qed.
 End Le.
+
+Module Eq.
+  Definition t {E A} {x : C.t E A} (u1 u2 : UseCase.t x) : Prop :=
+    Le.t u1 u2 /\ Le.t u2 u1.
+End Eq.
+
+Module Join.
+  Definition t {E A} {x : C.t E A} (u1 u2 : UseCase.t x) : UseCase.t x := {|
+    parameter := parameter u1 + parameter u2;
+    result := fun p =>
+      match p with
+      | inl p1 => result u1 p1
+      | inr p2 => result u2 p2
+      end;
+    run := fun p =>
+      match p with
+      | inl p1 => run u1 p1
+      | inr p2 => run u2 p2
+      end |}.
+
+  Lemma le_left {E A} {x : C.t E A} (u1 u2 : UseCase.t x)
+    : Le.t u1 (t u1 u2).
+    intro p1; exists (inl p1).
+    apply JMeq_refl.
+  Qed.
+
+  Lemma le_right {E A} {x : C.t E A} (u1 u2 : UseCase.t x)
+    : Le.t u2 (t u1 u2).
+    intro p2; exists (inr p2).
+    apply JMeq_refl.
+  Qed.
+
+  Lemma smallest_bound {E A} {x : C.t E A} (u1 u2 u3 : UseCase.t x)
+    (H1 : Le.t u1 u3) (H2 : Le.t u2 u3) : Le.t (t u1 u2) u3.
+    intro p.
+    destruct p as [p1 | p2].
+    - destruct (H1 p1) as [p3 H1'].
+      now exists p3.
+    - destruct (H2 p2) as [p3 H2'].
+      now exists p3.
+  Qed.
+End Join.
