dce66a2d0f7dd19ca69c965a14dea5b3042c1ff1
Guillaume Claret <dev@clarus.me>
2015-06-30 15:32:50 +0200

    Runs for the 'guess' function

--- src/GuessNumber.v
+++ src/GuessNumber.v
@@ -1,4 +1,5 @@
 (** A program to guess the number of the user using comparisons. *)
+Require Import Coq.Lists.List.
 Require Import Io.All.
 
 Import C.Notations.
@@ -28,8 +29,78 @@ Fixpoint guess {A} (mean : A -> A -> A) (steps : nat) (min max : A)
     let m := mean min max in
     let! answer := propose m in
     match answer with
-    | Lt => guess mean steps m max
-    | Gt => guess mean steps min m
+    | Lt => guess mean steps min m
+    | Gt => guess mean steps m max
     | Eq => ret (Some m)
     end
   end.
+
+Module Run.
+  Definition propose {A} (x : A) (c : comparison) : Run.t (propose x) c.
+    apply (Run.Call (E := E A) (Command.Propose x) c).
+  Defined.
+
+  Fixpoint never {A} (mean : A -> A -> A) (min max : A) (l : list bool)
+    : Run.t (guess mean (List.length l) min max) None.
+    destruct l as [|b l]; simpl.
+    - apply Run.Ret.
+    - destruct b.
+      + eapply Run.Let. apply (propose _ Lt).
+        apply never.
+      + eapply Run.Let. apply (propose _ Gt).
+        apply never.
+  Defined.
+
+  Definition simple_ok {A} (mean : A -> A -> A) (min max : A)
+    : Run.t (guess mean 1 min max) (Some (mean min max)).
+    eapply Run.Let. apply (propose _ Eq).
+    apply Run.Ret.
+  Defined.
+
+  Fixpoint ok {A} (mean : A -> A -> A) (min max : A) (l : list bool)
+    : {x : A & Run.t (guess mean (S (List.length l)) min max) (Some x)}.
+    destruct l; simpl.
+    - exists (mean min max).
+      eapply Run.Let. apply (propose _ Eq).
+      apply Run.Ret.
+    - destruct b.
+      + destruct (ok _ mean min (mean min max) l) as [x r].
+        exists x.
+        eapply Run.Let. apply (propose _ Lt).
+        apply r.
+      + destruct (ok _ mean (mean min max) max l) as [x r].
+        exists x.
+        eapply Run.Let. apply (propose _ Gt).
+        apply r.
+  Defined.
+
+  Fixpoint mean_n {A} (mean : A -> A -> A) (steps : nat) (min max : A) : A :=
+    match steps with
+    | O => mean min max
+    | S steps => mean_n mean steps min (mean min max)
+    end.
+
+  Fixpoint lts {A} (mean : A -> A -> A) (min max : A) (steps : nat)
+    : Run.t (guess mean (S steps) min max) (Some (mean_n mean steps min max)).
+    destruct steps as [|steps]; simpl.
+    - eapply Run.Let. apply (propose _ Eq).
+      apply Run.Ret.
+    - eapply Run.Let. apply (propose _ Lt).
+      apply lts.
+  Defined.
+
+  Module Nat.
+    Require Import Coq.Arith.Div2.
+
+    Definition mean (n m : nat) : nat :=
+      div2 (n + m).
+
+    Definition ex1 : Run.t (guess mean 5 0 10) (Some 4).
+      apply (Run.Let (propose 5 Lt)).
+      apply (Run.Let (propose 2 Gt)).
+      apply (Run.Let (propose 3 Gt)).
+      apply (Run.Let (propose 4 Eq)).
+      apply Run.Ret.
+    Defined.
+  End Nat.
+End Run.
